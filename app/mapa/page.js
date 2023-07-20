e141577aea5108638de4def4ed365e223909e4b0
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-20 10:50:45 -0600

    icons

--- app/mapa/page.js
+++ app/mapa/page.js
@@ -10,24 +10,24 @@ export default function Index() {
 
       {/* Navbar */}
       <nav className = "bg-black z-10 fixed w-full bottom-0">
-        <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+        <div className="container mx-auto px-6 py-1 flex justify-between items-center">
 
           {/* Home */}
-          <Link href="/" className="flex flex-col items-center text-xs text-white">
+          <Link href="/" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-black">
             <Image src="/inicio.png" alt="Home" width={40} height={40} />
             <span className="mt-0">Inicio</span>
           </Link>
 
           {/* Mapa */}
           <Link href="/mapa" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-white">
-            <Image src="/mapa.png" alt="Mapa" width={25} height={25} />
-            <span className="mt-2">Mapa</span>
+            <Image src="/inicio.png" alt="Mapa" width={40} height={40} />
+            <span className="mt-0">Mapa</span>
           </Link>
         
           {/* Datos */}
-          <Link href="/datos" className="flex flex-col items-center text-xs text-white">
-            <Image src="/datos.png" alt="Datos" width={30} height={30} />
-            <span className="mt-2">Datos</span>
+          <Link href="/datos" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-black">
+            <Image src="/inicio.png" alt="Datos" width={40} height={40} />
+            <span className="mt-0">Datos</span>
           </Link>
 
         </div>
--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Index() {
     <div>
       {/* Navbar */}
       <nav className = "bg-black z-10 fixed w-full bottom-0">
-        <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+        <div className="container mx-auto px-6 py-1 flex justify-between items-center">
 
           {/* Home */}
           <Link href="/" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-white">
@@ -17,15 +17,15 @@ export default function Index() {
           </Link>
 
           {/* Mapa */}
-          <Link href="/mapa" className="flex flex-col items-center text-xs text-white">
-            <Image src="/mapa.png" alt="Mapa" width={25} height={25} />
-            <span className="mt-2">Mapa</span>
+          <Link href="/mapa" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-black">
+            <Image src="/inicio.png" alt="Mapa" width={40} height={40} />
+            <span className="mt-0">Mapa</span>
           </Link>
         
           {/* Datos */}
-          <Link href="/datos" className="flex flex-col items-center text-xs text-white">
-            <Image src="/datos.png" alt="Datos" width={30} height={30} />
-            <span className="mt-2">Datos</span>
+          <Link href="/datos" className="flex flex-col items-center text-xs text-white border-b-4 pb-2 border-black">
+            <Image src="/inicio.png" alt="Datos" width={40} height={40} />
+            <span className="mt-0">Datos</span>
           </Link>
 
         </div>
