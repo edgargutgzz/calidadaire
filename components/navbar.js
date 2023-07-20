81b452478d418cd583d1b601a93d6ea1f318ed08
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-20 17:47:28 -0600

    .

--- components/navbar.js
+++ components/navbar.js
@@ -11,7 +11,7 @@ function Navbar({ currentPage }) {
           <Link href="/">
             <div className="flex flex-col items-center text-xs text-white">
               <Image src={currentPage === 'inicio' ? "/inicio_on.png" : "/inicio_off.png"} alt="Inicio" width={25} height={25} />
-              <span className="mt-0 pt-2">Inicio</span>
+              <span className="mt-0 pt-1">Inicio</span>
             </div>
           </Link>
 
