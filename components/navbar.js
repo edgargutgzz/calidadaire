adee7a70ce3df8d68d69292e080c1cc9b99c7e03
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-21 12:28:02 -0600

    icons

--- components/navbar.js
+++ components/navbar.js
@@ -4,12 +4,12 @@ import Image from 'next/image'
 
 function Navbar({ currentPage }) {
   return (
-    <nav className = "bg-grey z-10 fixed w-full bottom-0">
+    <nav className = "bg-white z-10 fixed w-full bottom-0">
         <div className="container mx-auto px-6 py-2 flex justify-between items-center">
 
           {/* Home */}
           <Link href="/">
-            <div className="flex flex-col items-center text-xs text-white">
+            <div className="flex flex-col items-center text-xs text-black">
               <Image src={currentPage === 'inicio' ? "/inicio_on.png" : "/inicio_off.png"} alt="Inicio" width={25} height={25} />
               <span className="mt-0 pt-1">Inicio</span>
             </div>
@@ -17,7 +17,7 @@ function Navbar({ currentPage }) {
 
           {/* Mapa */}
           <Link href="/mapa">
-            <div className="flex flex-col items-center text-xs text-white">
+            <div className="flex flex-col items-center text-xs text-black">
               <Image src={currentPage === 'mapa' ? "/mapa_on.png" : "/mapa_off.png"} alt="Mapa" width={26} height={26} />
               <span className="mt-0 pt-1">Mapa</span>
             </div>
@@ -25,7 +25,7 @@ function Navbar({ currentPage }) {
         
           {/* Datos */}
           <Link href="/datos">
-            <div className="flex flex-col items-center text-xs text-white">
+            <div className="flex flex-col items-center text-xs text-black">
               <Image src={currentPage === 'datos' ? "/datos_on.png" : "/datos_off.png"} alt="Datos" width={26} height={26} />
               <span className="mt-0 pt-1">Datos</span>
             </div>
