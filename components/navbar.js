62dda7864fea0b3294d6383ae3a69a84ad76a27c
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-24 12:40:06 -0600

    .

--- components/navbar.js
+++ components/navbar.js
@@ -9,8 +9,8 @@ function Navbar({ currentPage }) {
 
           {/* Home */}
           <Link href="/">
-            <div className="flex flex-col items-center text-xs text-black mx-2">
-              <Image src={currentPage === 'inicio' ? "/inicio_on.png" : "/inicio_off.png"} alt="Inicio" width={24} height={24} />
+            <div className="flex flex-col items-center text-xs text-black mx-0">
+              <Image src={currentPage === 'inicio' ? "/inicio_on.png" : "/inicio_off.png"} alt="Inicio" width={25} height={25} />
               <span className={`mt-0 pt-1 ${currentPage === 'inicio' ? "font-bold" : ""}`}>Inicio</span>
             </div>
           </Link>
@@ -18,15 +18,15 @@ function Navbar({ currentPage }) {
           {/* Mapa */}
           <Link href="/mapa">
             <div className="flex flex-col items-center text-xs text-black mx-16">
-              <Image src={currentPage === 'mapa' ? "/mapa_on.png" : "/mapa_off.png"} alt="Mapa" width={24} height={24} />
+              <Image src={currentPage === 'mapa' ? "/mapa_on.png" : "/mapa_off.png"} alt="Mapa" width={25} height={25} />
               <span className={`mt-0 pt-1 ${currentPage === 'mapa' ? "font-bold" : ""}`}>Mapa</span>
             </div>
           </Link>
         
           {/* Datos */}
           <Link href="/datos">
-            <div className="flex flex-col items-center text-xs text-black mx-2">
-              <Image src={currentPage === 'datos' ? "/datos_on.png" : "/datos_off.png"} alt="Datos" width={24} height={24} />
+            <div className="flex flex-col items-center text-xs text-black mx-0">
+              <Image src={currentPage === 'datos' ? "/datos_on.png" : "/datos_off.png"} alt="Datos" width={25} height={25} />
               <span className={`mt-0 pt-1 ${currentPage === 'datos' ? "font-bold" : ""}`}>Datos</span>
             </div>
           </Link>
