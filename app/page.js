5d48af520e47a15cf038174ea36ed250dff28061
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-24 10:25:40 -0600

    quality bar

--- app/page.js
+++ app/page.js
@@ -4,6 +4,29 @@ import React, { useState } from 'react';
 import Navbar from '../components/navbar';
 import Mapa, {getMarkerColor} from '../components/mapa';
 
+// QualityBar
+function QualityBar({ pm25 }) {
+  const calculateLeft = () => {
+    if (pm25 <= 25) return (pm25 / 25) * 20;  // 0-25 range is 20% of total
+    if (pm25 <= 45) return 20 + ((pm25 - 26) / 20) * 20;  // 26-45 range is 20% of total
+    if (pm25 <= 79) return 40 + ((pm25 - 46) / 34) * 20;  // 46-79 range is 20% of total
+    if (pm25 <= 147) return 60 + ((pm25 - 80) / 68) * 20;  // 80-147 range is 20% of total
+    return 100; // for PM2.5 > 147
+  };
+
+  return (
+    <div className="relative flex border-b-2 border-gray-300 min-h-[10px] rounded-lg overflow-hidden">
+      <div style={{ flexBasis: "20%" }} className="bg-green-500" />
+      <div style={{ flexBasis: "20%" }} className="bg-yellow-500" />
+      <div style={{ flexBasis: "20%" }} className="bg-orange-500" />
+      <div style={{ flexBasis: "20%" }} className="bg-red-500" />
+      <div style={{ flexBasis: "20%" }} className="bg-purple-500" />
+      <div style={{ left: `${calculateLeft()}%` }} className="absolute w-3 h-3 bg-white rounded-full border-2 border-gray-300 transform -translate-y-1/2 top-1/2"></div>
+    </div>
+  );
+}
+
+// Index
 export default function Index() {
   const [nearestSensor, setNearestSensor] = useState(null);
 
@@ -24,17 +47,18 @@ export default function Index() {
       
       {/* Calidad del Aire */}
       {nearestSensor && (
-        <div className={`mx-4 p-4 my-10 rounded-lg text-white ${getMarkerColor(nearestSensor.pm25)}`}>
-          <p>Nearest Sensor PM2.5: {nearestSensor.pm25 != null ? Math.round(nearestSensor.pm25) : 'N/A'}</p>
-        </div>
+        <>
+          {/* <div className={`mx-4 p-4 mb-10 rounded-lg text-white ${getMarkerColor(nearestSensor.pm25)}`}>
+            <p>Nearest Sensor PM2.5: {nearestSensor.pm25 != null ? Math.round(nearestSensor.pm25) : 'N/A'}</p>
+          </div> */}
+          <div className="mx-4 mb-2 pt-10">
+          <QualityBar pm25={nearestSensor.pm25} />
+          </div>
+        </>
       )}
     </div>
   );
 }
-
-
-
-
 
 
 
