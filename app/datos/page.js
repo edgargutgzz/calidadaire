786c89cd996078779b22a54d69b7858fed31e5dd
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-27 17:33:36 -0600

    dropdown

--- app/datos/page.js
+++ app/datos/page.js
@@ -1,13 +1,41 @@
+"use client";
+
+import { useState } from 'react';
 import Navbar from '../../components/navbar';
 
 export default function Index() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <Navbar currentPage="datos" />
-      <h1 className="text-4xl text-center mt-20">Datos</h1>
+      <div className="mx-4 mb-2 pt-4">
+        <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
+          <div onClick={toggleOpen} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
+            <p>¿Por qué utilizar esta app sobre las otras?</p>
+            <img 
+              src={isOpen ? "/up-arrow.png" : "/down-arrow.png"} 
+              alt="toggle" 
+              className="h-4 w-4" 
+            />
+          </div>
+          {isOpen && (
+            <p className="mt-2 text-sm">
+              Esta app es la única que te permite saber si puedes salir a correr o andar en bici, si puedes llevar a tus hijos o a tus padres a un parque, o si puedes hacer un picnic en el parque. Todo esto basado en la calidad del aire en tu ciudad.
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
+
+
+
 
 
 
