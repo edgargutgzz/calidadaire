4a49ad2cf96f1df6897f046f8431da3122b58dce
Edgar Gutierrez <edgargu47@gmail.com>
2023-07-31 17:09:48 -0600

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import Navbar from '../components/navbar'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   )
