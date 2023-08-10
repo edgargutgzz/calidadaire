f92257719a332ded89a32943639728362e59f0ea
Edgar Gutierrez <edgargu47@gmail.com>
2023-08-10 12:57:57 -0600

    download button first v

--- app/datos/page.js
+++ app/datos/page.js
@@ -1,7 +1,12 @@
 "use client";
 
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import Papa from 'papaparse';
 import Navbar from '../../components/navbar';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 
 export default function Index() {
   const [isOpen1, setIsOpen1] = useState(false);
@@ -9,10 +14,52 @@ export default function Index() {
   const [isOpen3, setIsOpen3] = useState(false);
   const [isOpen4, setIsOpen4] = useState(false);
 
+  const downloadData = async () => {
+    let data = [];
+    let hasMore = true;
+    let lastId = null;
+  
+    while (hasMore) {
+      const query = supabase.from('calidad_aire').select('*').order('id', { ascending: true }).limit(1000);
+      if (lastId) {
+        query.gt('id', lastId);
+      }
+  
+      const { data: pageData, error } = await query;
+  
+      if (error) {
+        console.error('Error fetching data:', error);
+        return;
+      }
+  
+      if (pageData.length === 0) {
+        hasMore = false;
+      } else {
+        lastId = pageData[pageData.length - 1].id;
+        data = [...data, ...pageData];
+      }
+    }
+  
+    const csv = Papa.unparse(data);
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'calidad_aire.csv');
+    document.body.appendChild(link);
+    link.click();
+  };
+  
+  
+  
   return (
     <div className="flex flex-col lg:flex-row h-screen">
       <Navbar currentPage="datos" />
       <div className="mx-4 pt-4 flex-grow overflow-auto lg:w-9/12 lg:flex-grow lg:pl-4">
+        {/* Download button */}
+        <button onClick={downloadData} className="bg-blue-500 text-white rounded-lg p-2 mb-4 cursor-pointer">
+          Descargar datos de calidad del aire
+        </button>
         {/* Pregunta 1 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
           <div onClick={() => setIsOpen1(!isOpen1)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
