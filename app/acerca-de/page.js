79cb4b14244600f8efea8b17e2afffc833d3fe93
Edgar Gutierrez <edgargu47@gmail.com>
2023-08-18 07:48:33 -0600

    .

--- app/acerca-de/page.js
+++ app/acerca-de/page.js
@@ -19,13 +19,32 @@ export default function Index() {
 
         {/* Logo */}
         <div className="mx-1 mt-2 mb-6 flex justify-center">
-        <img src="/aire_claro.png" alt="Aire Claro" className="w-40" /> {/* Adjust the size here */}
+          <img src="/aire_claro.png" alt="Aire Claro" className="w-40" />
         </div>
 
         {/* Pregunta 1 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
-          <div onClick={() => setIsOpen2(!isOpen2)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
+          <div onClick={() => setIsOpen1(!isOpen1)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
             <p className='mr-4'>¿Qué información utilizamos para medir la calidad del aire?</p>
+            <img 
+              src={isOpen1 ? "/up-arrow.png" : "/down-arrow.png"} // Corrected here
+              alt="toggle" 
+              className="h-3 w-3" 
+            />
+          </div>
+          {isOpen1 && (
+            <p className="mt-2 text-sm">
+              Utilizamos 2 fuentes principales: <br />
+              <div className="py-1">1. Las estaciones de monitoreo de la <a href="http://aire.nl.gob.mx/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Secretaría de Desarrollo Sustentable</a> del estado de Nuevo León.</div>
+              <div className="py-1">2. Las estaciones de monitoreo ciudadanas de <a href="https://www2.purpleair.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Purple Air</a>.</div>
+            </p>
+          )}
+        </div>
+  
+        {/* Pregunta 2 */}
+        <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
+          <div onClick={() => setIsOpen2(!isOpen2)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
+            <p className='mr-4'>¿Quién es el Observatorio Ciudadano de la Calidad del Aire?</p>
             <img 
               src={isOpen2 ? "/up-arrow.png" : "/down-arrow.png"} 
               alt="toggle" 
@@ -34,14 +53,48 @@ export default function Index() {
           </div>
           {isOpen2 && (
             <p className="mt-2 text-sm">
-              Utilizamos 2 fuentes principales: <br />
-              <div className="py-1">1. Las estaciones de monitoreo de la <a href="http://aire.nl.gob.mx/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Secretaría de Desarrollo Sustentable</a> del estado de Nuevo León.</div>
-              <div className="py-1">2. Las estaciones de monitoreo ciudadanas de <a href="https://www2.purpleair.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Purple Air</a>.</div>
+              El <a href="https://observatoriodelaire.com/index.html" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">OCCAMM</a> es un instrumento ciudadano confiable, que promueve información sobre el estado, causas y efectos de la contaminación del Aire en la Zona Metropolitana.
             </p>
           )}
         </div>
+
+        {/* Pregunta 3 */}
+        <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
+          <div onClick={() => setIsOpen3(!isOpen3)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
+            <p className='mr-4'>¿Quién es Data Comun?</p>
+            <img
+              src={isOpen3 ? "/up-arrow.png" : "/down-arrow.png"}
+              alt="toggle"
+              className="h-3 w-3"
+            />
+          </div>
+          {isOpen3 && (
+            <p className="mt-2 text-sm">
+              <a href="https://www.datacomun.org/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Data Comun</a> es una empresa de tecnología que promueve el uso de datos abiertos para crear una ciudad más segura, justa y sustentable.
+            </p>
+          )}
+        </div>
+
+        {/* Pregunta 4 */}
+        <div className="bg-white rounded-lg shadow-lg p-4 mb-4">
+          <div onClick={() => setIsOpen4(!isOpen4)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
+            <p className='mr-4'>¿Cómo se calcula el índice de calidad del aire?</p>
+            <img
+              src={isOpen4 ? "/up-arrow.png" : "/down-arrow.png"}
+              alt="toggle"
+              className="h-3 w-3"
+            />
+          </div>
+          {isOpen4 && (
+            <p className="mt-2 text-sm">
+              El índice de calidad del aire se calcula con base en los niveles de contaminantes en el aire y sus efectos en la salud. El índice de calidad del aire se divide en 6 categorías, que van de Bueno a Peligroso.
+            </p>
+          )}
+        </div>
+        
 
       </div>
     </div>
   );
 }
+
