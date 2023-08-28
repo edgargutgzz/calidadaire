7894a5d7f9368868dcac511b06433cbcca78baa2
Edgar Gutierrez <edgargu47@gmail.com>
2023-08-28 17:54:38 -0600

    .

--- app/acerca-de/page.js
+++ app/acerca-de/page.js
@@ -27,7 +27,7 @@ export default function Index() {
         {/* Pregunta 1 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen3(!isOpen3)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Qué es Aire Claro?</p>
+            <p className='text-base lg:text-base'>¿Qué es Aire Claro?</p>
             <img
               src={isOpen3 ? "/up-arrow.png" : "/down-arrow.png"}
               alt="toggle"
@@ -35,7 +35,7 @@ export default function Index() {
             />
           </div>
           {isOpen3 && (
-            <p className="mt-4 text-base">
+            <p className="mt-4 text-base lg:text-base">
               Aire Claro es la plataforma que muestra la calidad del aire en tiempo real de la Zona Metropolitana de Monterrey. <br />
             </p>
           )}
@@ -44,7 +44,7 @@ export default function Index() {
         {/* Pregunta 2 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen4(!isOpen4)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Cómo garantizan la precisión de sus recomendaciones?</p>
+            <p className='text-base lg:text-base'>¿Cómo garantizan la precisión de sus recomendaciones?</p>
             <img
               src={isOpen4 ? "/up-arrow.png" : "/down-arrow.png"}
               alt="toggle"
@@ -52,7 +52,7 @@ export default function Index() {
             />
           </div>
           {isOpen4 && (
-            <p className="mt-4 text-base">
+            <p className="mt-4 text-base lg:text-base">
               Medimos sólo las partículas más dañinas para tu salud de acuerdo a las condiciones climatológicas de la Zona Metropolitana de Monterrey.
             </p>
           )}
@@ -61,7 +61,7 @@ export default function Index() {
         {/* Pregunta 3 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen6(!isOpen6)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Quién conforma la población sensible?</p>
+            <p className='text-base lg:text-base'>¿Quién conforma la población sensible?</p>
             <img
               src={isOpen4 ? "/up-arrow.png" : "/down-arrow.png"}
               alt="toggle"
@@ -69,7 +69,7 @@ export default function Index() {
             />
           </div>
           {isOpen6 && (
-            <p className="mt-4 text-base">
+            <p className="mt-4 text-base lg:text-base">
               Infantes, mujeres embarazadas, personas mayores o con padecimientos.
             </p>
           )}
@@ -78,7 +78,7 @@ export default function Index() {
         {/* Pregunta 4 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen1(!isOpen1)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Qué información utilizan para medir la calidad del aire?</p>
+            <p className='text-base lg:text-base'>¿Qué información utilizan para medir la calidad del aire?</p>
             <img 
               src={isOpen1 ? "/up-arrow.png" : "/down-arrow.png"} // Corrected here
               alt="toggle" 
@@ -86,7 +86,7 @@ export default function Index() {
             />
           </div>
           {isOpen1 && (
-            <p className="mt-4 text-base">
+            <p className="mt-4 text-base lg:text-base">
               <div className="py-1">1. Las estaciones de monitoreo de la <a href="http://aire.nl.gob.mx/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Secretaría de Desarrollo Sustentable</a> del estado de Nuevo León.</div>
               <div className="py-1">2. Las estaciones de monitoreo ciudadanas de <a href="https://www2.purpleair.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Purple Air</a>.</div>
             </p>
@@ -96,7 +96,7 @@ export default function Index() {
         {/* Pregunta 5 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen2(!isOpen2)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Quién es el Observatorio Ciudadano de la Calidad del Aire?</p>
+            <p className='text-base lg:text-base'>¿Quién es el Observatorio Ciudadano de la Calidad del Aire?</p>
             <img
               src={isOpen2 ? "/up-arrow.png" : "/down-arrow.png"}
               alt="toggle"
@@ -105,10 +105,10 @@ export default function Index() {
           </div>
           {isOpen2 && (
             <>
-              <p className="mt-4 text-base">
+              <p className="mt-4 text-base lg:text-base">
                 El <a href="https://observatoriodelaire.com/index.html" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">OCCAMM</a> es un organización de la sociedad civil que promueve información sobre el estado, causas y efectos de la contaminación del aire en la Zona Metropolitana de Monterrey.
               </p>
-              <p className="mt-2 text-base">
+              <p className="mt-2 text-base lg:text-base">
                 El observatorio es también quién evalúa la información que utiliza Aire Claro para brindar las mejores recomendaciones posibles.
               </p>
             </>
@@ -118,7 +118,7 @@ export default function Index() {
         {/* Pregunta 6 */}
         <div className="bg-white rounded-lg shadow-lg p-4 mb-6">
           <div onClick={() => setIsOpen5(!isOpen5)} className="mt-2 text-sm font-bold cursor-pointer flex justify-between items-center">
-            <p className='text-base'>¿Tienes más preguntas o comentarios?</p>
+            <p className='text-base lg:text-base'>¿Tienes más preguntas o comentarios?</p>
             <img 
               src={isOpen5 ? "/up-arrow.png" : "/down-arrow.png"} 
               alt="toggle" 
@@ -126,7 +126,7 @@ export default function Index() {
             />
           </div>
           {isOpen5 && (
-            <p className="mt-4 text-base">
+            <p className="mt-4 text-base lg:text-base">
               Envíanos un correo  <a href="mailto:hola@aireclaro.com" className="text-blue-500 hover:underline">hola@aireclaro.com</a>
             </p>
           )}
