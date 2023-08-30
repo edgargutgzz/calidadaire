b3646e65960674aa698a6e4dbdd062991e0926d9
Edgar Gutierrez <edgargu47@gmail.com>
2023-08-30 12:01:02 -0600

    .

--- app/page.js
+++ app/page.js
@@ -130,7 +130,7 @@ export default function Recomendaciones() {
           {/* Adultos Mayores */}
           <div className={`bg-white rounded-lg shadow-lg p-4 mb-4 w-1/2 mr-2 ${getBorderColor(nearestSensor.pm25)} border-b-4`}>
             <div className="flex items-center space-x-2">
-              <img src="adultos_mayores.png" width={26} height={26} alt="Emabarazadas Icon" />
+              <img src="adultos_mayores.png" width={28} height={28} alt="Adultos Mayores Icon" />
             </div>
             <p className="mt-2 text-sm lg:text-sm font-semibold">Adultos Mayores</p>
             <p className="mt-1 text-sm lg:text-base">{getActivityRecommendation('adultos_mayores', nearestSensor.pm25)}</p>
@@ -138,8 +138,8 @@ export default function Recomendaciones() {
           {/* Condiciones Médicas */}
           <div className={`bg-white rounded-lg shadow-lg p-4 mb-4 w-1/2 ml-2 ${getBorderColor(nearestSensor.pm25)} border-b-4`}>
             <div className="flex items-center space-x-2">
-              <img src="corazon.png" width={26} height={26} alt="Emabarazadas Icon" />
-              <img src="pulmon.png" width={26} height={26} alt="Emabarazadas Icon" />
+              <img src="corazon.png" width={28} height={28} alt="Emabarazadas Icon" />
+              <img src="pulmon.png" width={28} height={28} alt="Emabarazadas Icon" />
             </div>
             <p className="mt-2 text-sm lg:text-sm font-semibold">Condiciones Médicas</p>
             <p className="mt-1 text-sm lg:text-base">{getActivityRecommendation('condiciones_medicas', nearestSensor.pm25)}</p>
