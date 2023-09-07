6bd6c4ff72124d8817f70f648bbc070c0d4374bf
Edgar Gutierrez <edgargu47@gmail.com>
2023-09-07 10:51:45 -0600

    .

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ function QualityBar({ pm25 }) {
     <>
       <div className="text-left text-gray-400 text-xs lg:text-sm">CALIDAD DEL AIRE</div>
       <div className="text-left font-semibold text-4xl lg:text-5xl mt-1">{getQualityText()}</div>
-      <div className="text-left text-base mt-2">{getRecommendedText()}</div> 
+      <div className="text-left text-base lg:text-lg mt-2">{getRecommendedText()}</div> 
       <div className="relative flex min-h-[12px] rounded-lg overflow-hidden mt-4 mb-1">
         <div style={{ flexBasis: "12%" }} className="bg-custom-green" />
         <div style={{ flexBasis: "10%" }} className="bg-gradient-to-r from-custom-green to-custom-yellow" />
