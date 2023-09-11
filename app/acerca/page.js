32797fc341d34a99c00481c2069e67c3a40a26a9
Edgar Gutierrez <edgargu47@gmail.com>
2023-09-11 12:59:55 -0600

    .

--- app/acerca/page.js
+++ app/acerca/page.js
@@ -232,7 +232,7 @@ export default function Index() {
         {/* OCCAMM */}
         <div className="mt-12 mb-8 lg:mt-16 text-center">
           <div className="flex items-center justify-center">
-            <div className="text-gray-400 text-xs lg:text-sm pl-0 pr-2">Un proyecto del</div>
+            <div className="text-gray-400 text-xs lg:text-sm pl-0 pr-1">Un proyecto del</div>
             <img src="/logo_occamm.png" alt="Observatorio del Aire" className="ml-4" style={{ width: "180px", height: "auto" }} />
           </div>
         </div>
